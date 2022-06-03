92a88094a48900070f88b834efe661556932a244
Ma. Angelica Sevilla <angelmaesevilla@gmail.com>
2022-06-03 13:35:51 +0800

    Winner checking at horizontal works!

--- Tic-tac-toe/Form2.cs
+++ Tic-tac-toe/Form2.cs
@@ -24,7 +24,9 @@ namespace Tic_tac_toe
             {
                 if (control is Button)
                 {
+#pragma warning disable CS8622 // Nullability of reference types in type of parameter doesn't match the target delegate (possibly because of nullability attributes).
                     control.Click += new System.EventHandler(btnclick);
+#pragma warning restore CS8622 // Nullability of reference types in type of parameter doesn't match the target delegate (possibly because of nullability attributes).
                 }
             }
         }
@@ -40,11 +42,13 @@ namespace Tic_tac_toe
                 {
                     btn.Text = "X";
                     textBox1.Text = "Player O 's  turn now";
+                    Winnercheck();
                 }
                 else
                 {
                     btn.Text = "O";
                     textBox1.Text = "Player X 's turn now";
+                    Winnercheck();
                 }
 
                 XorO++;
@@ -56,47 +60,19 @@ namespace Tic_tac_toe
             //horizontal winning check
             if (!btn1.Text.Equals("") && btn1.Text.Equals(btn2.Text) && btn1.Text.Equals(btn3.Text))
             {
+                winlayout(btn1, btn2, btn3);
                 win = true;
             }
             if (!btn4.Text.Equals("") && btn4.Text.Equals(btn5.Text) && btn4.Text.Equals(btn6.Text))
             {
+                winlayout(btn4, btn5, btn6);
                 win = true;
             }
             if (!btn7.Text.Equals("") && btn7.Text.Equals(btn8.Text) && btn7.Text.Equals(btn9.Text))
             {
+                winlayout(btn7, btn8, btn9);
                 win = true;
             }
-
-            //vertical winning check
-            if (!btn1.Text.Equals("") && btn1.Text.Equals(btn4.Text) && btn1.Text.Equals(btn7.Text))
-            {
-                win = true;
-            }
-            if (!btn2.Text.Equals("") && btn2.Text.Equals(btn5.Text) && btn2.Text.Equals(btn8.Text))
-            {
-                win = true;
-            }
-            if (!btn3.Text.Equals("") && btn3.Text.Equals(btn6.Text) && btn3.Text.Equals(btn9.Text))
-            {
-                win = true;
-            }
-
-            //diagonal winning check
-            if (!btn1.Text.Equals("") && btn1.Text.Equals(btn5.Text) && btn1.Text.Equals(btn9.Text))
-            {
-                win = true;
-            }
-            if (!btn3.Text.Equals("") && btn3.Text.Equals(btn5.Text) && btn3.Text.Equals(btn7.Text))
-            {
-                win = true;
-            }
-            //button text length function would tell the winning check as a draw.
-            if (AllBtnLength() == 9 && win == false)
-            {
-                textBox1.Text = " No one won :< ";
-                MessageBox.Show(" Aww, it's a draw >,< ");
-            }
-
         }
         // get all button text length function -> return int
         public int AllBtnLength()
@@ -110,6 +86,20 @@ namespace Tic_tac_toe
                 }
             }
             return allTextButtonsLength;
+        }
+        // win layout function is added for easier view of the Player's winning line
+        public void winlayout(Button b1, Button b2, Button b3)
+        {
+            b1.BackColor = Color.Thistle;
+            b2.BackColor = Color.Thistle;
+            b3.BackColor = Color.Thistle;
+
+            b1.ForeColor = Color.DarkMagenta;
+            b2.ForeColor = Color.DarkMagenta;
+            b3.ForeColor = Color.DarkMagenta;
+
+            textBox1.Text = b1.Text + " You Won! :>";
+            MessageBox.Show(" You Won! Good job!");
         }
 
     }
