cd4998d3fa3a7c2d53d30e90d5e1571e91693394
Ma. Angelica Sevilla <angelmaesevilla@gmail.com>
2022-06-03 13:40:08 +0800

    Winner checking at diagonal line works!

--- Tic-tac-toe/Form2.cs
+++ Tic-tac-toe/Form2.cs
@@ -90,6 +90,19 @@ namespace Tic_tac_toe
                 winlayout(btn3, btn6, btn9);
                 win = true;
             }
+
+            //diagonal winning check
+            if (!btn1.Text.Equals("") && btn1.Text.Equals(btn5.Text) && btn1.Text.Equals(btn9.Text))
+            {
+                winlayout(btn1, btn5, btn9);
+                win = true;
+            }
+            if (!btn3.Text.Equals("") && btn3.Text.Equals(btn5.Text) && btn3.Text.Equals(btn7.Text))
+            {
+                winlayout(btn3, btn5, btn7);
+                win = true;
+            }
+
         }
         // get all button text length function -> return int
         public int AllBtnLength()
