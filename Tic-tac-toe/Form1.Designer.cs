11444e187c9ec4cc28ecdd4ec58e60bb0816d62c
Ma. Angelica Sevilla <angelmaesevilla@gmail.com>
2022-06-03 11:38:18 +0800

    button3 (instruction) is working.

--- Tic-tac-toe/Form1.Designer.cs
+++ Tic-tac-toe/Form1.Designer.cs
@@ -81,6 +81,7 @@
             this.button3.TabIndex = 3;
             this.button3.Text = "Instruction";
             this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Form1
             // 
--- Tic-tac-toe/Form1.cs
+++ Tic-tac-toe/Form1.cs
@@ -12,5 +12,11 @@ namespace Tic_tac_toe
             MessageBox.Show("This is a Tic-Tac-Toe program made by M.Sevilla." +
                 "This is played by X and O symbols.");
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("In order for the player to win, it is a must to succeed in markering three cells in a straight line at any direction (by horizonally, vertically, and diagobally)." +
+                " Have fun playing!");
+        }
     }
 }
